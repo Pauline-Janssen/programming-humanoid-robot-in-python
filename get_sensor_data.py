f082bdde3cd480a4f05a5ae595b92f66a3594a55
Pauline-Janssen <75090148+Pauline-Janssen@users.noreply.github.com>
2021-11-07 11:42:04 +0100

    Add files via upload
    1. HA, get and set angle and temperature of arm and headyaw

---
+++ get_sensor_data.py
@@ -0,0 +1,36 @@
+'''
+In this exercise you need to know how to get sensor data.
+
+* Task: get the current joint angle and temperature of joint HeadYaw
+
+* Hint: The current sensor data of robot are stored in perception (class Perception in spark_agent.py)
+
+'''
+
+# add PYTHONPATH
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
+
+from spark_agent import SparkAgent
+
+
+class MyAgent(SparkAgent):
+    def think(self, perception):
+        # lass MyAgent erbt von class SparkAgent
+        angle = 0
+        temperature = 0
+        # YOUR CODE HERE
+        angle = self.perception.joint['HeadYaw']
+        temperature = self.perception.joint_temperature['HeadYaw']
+        # '''hj1 ist HeadYaw, hj1 gibt es nicht im dict warum auch immer, string aufpassen'''
+        # get angle and temperature to current data of joint HeadYaw
+
+        print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
+        return super(MyAgent, self).think(perception)
+
+
+if '__main__' == __name__:
+    agent = MyAgent()
+    agent.run()
