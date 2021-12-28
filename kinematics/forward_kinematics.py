6b01f14b1519d4c61caf36d02145f4f7be56ed4f
Pauline-Janssen <75090148+Pauline-Janssen@users.noreply.github.com>
2021-12-28 11:14:06 +0100

    homework kinematics

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -19,6 +19,9 @@
 # add PYTHONPATH
 import os
 import sys
+import numpy as np
+import math
+from numpy import sin, cos, pi, matrix, random
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
 from numpy.matlib import matrix, identity
@@ -36,7 +39,10 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
                        # YOUR CODE HERE
                        }
 
@@ -54,6 +60,69 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         '''
         T = identity(4)
         # YOUR CODE HERE
+        #joint_angle = np.pi / 2
+        dicti_trans ={'HeadYaw': [0,0,126.5], 'HeadPitch': [0,0,0], 'LShoulderPitch': [0,98,100], 'LShoulderRoll':[0,0,0],
+                      'LElbowYaw': [105,15,0], 'LElbowRoll': [0,0,0],
+                      'LHipYawPitch': [0,50,-85], 'LHipRoll': [0,0,0], 'LHipPitch':[0,0,0], 'LKneePitch':[0,0,-100], 'LAnklePitch':[0,0,-102.9], 'LAnkleRoll':[0,0,0],
+                      'RHipYawPitch': [0,-50,-85], 'RHipRoll':[0,0,0], 'RHipPitch':[0,0,0], 'RKneePitch':[0,0,-100], 'RAnklePitch':[0,0,-102.9],
+                      'RAnkleRoll':[0,0,0], 'RShoulderPitch':[0,-98,100], 'RShoulderRoll': [0,0,0], 'RElbowYaw':[105,-15,0], 'RElbowRoll':[0,0,0]}
+        '''dicti_rot = {'HeadYaw': 3, 'HeadPitch':2, 'LShoulderPitch':2, 'LShoulderRoll':1, # 1 roll, 2 pitch, 3 yaw rotation
+                      'LElbowYaw':3, 'LElbowRoll':1, 'LWristYaw':3, 'LHipYawPitch':2,
+                      'LHipRoll':1, 'LHipPitch':2, 'LKneePitch':2, 'LAnklePitch':2, 'LAnkleRoll':1, 'RHipYawPitch':2,
+                      'RHipRoll':1, 'RHipPitch':2, 'RKneePitch':2, 'RAnklePitch':2,
+                      'RAnkleRoll':1, 'RShoulderPitch':2, 'RShoulderRoll':1, 'RElbowYaw':3, 'RElbowRoll':1, 'RWristYaw':3}'''
+        c = cos(joint_angle)
+        s = sin(joint_angle)
+
+        R_x = matrix([[1, 0, 0],
+                      [0, c, -s],
+                      [0, s, c]])
+        R_y = matrix([[c, 0, s],
+                      [0, 1, 0],
+                      [-s, 0, c]])
+        R_z = matrix([[c, s, 0],
+                      [-s, c, 0],
+                      [0, 0, 1]])
+        if joint_name == 'HipYawPitch':  #spezialfall weil joint um spezielle achse dreht
+            #hip_offset_z = 85/1000 # unit vector (x,y,z)=(l,m,n), l = 0
+            #hip_offset_y = 50/1000
+            term = 1-c
+            m = math.sqrt(2) #rot achse ist 45° also (0,sqrt(2), sqrt(2)), l = 0
+            n = math.sqrt(2)
+            T = matrix([[l*l*term+c, -n*s, m*s],
+                        [n*s, m*m*term+c, n*m*term],
+                        [-m*s, m*n*term, n*n*term+c]])
+            row = dicti_trans[joint_name]
+            newrow = [x / 1000 for x in row]
+            T = np.vstack([T, newrow])
+            newcolumn = [[0], [0], [0], [1]]
+            T = np.hstack([T, newcolumn])
+        if 'Pitch' in joint_name:
+            T = R_y
+            row = dicti_trans[joint_name]
+            newrow = [x / 1000 for x in row]
+            T = np.vstack([T, newrow])
+            newcolumn = [[0], [0], [0], [1]]
+            T = np.hstack([T, newcolumn])
+            #print('pitch')
+
+        if 'Roll' in joint_name:
+            T = R_x
+            row = dicti_trans[joint_name]
+            newrow = [x / 1000 for x in row]
+            T = np.vstack([T, newrow])
+            newcolumn = [[0], [0], [0], [1]]
+            T = np.hstack([T, newcolumn])
+
+        if 'Yaw' in joint_name:
+            T = R_z
+            row = dicti_trans[joint_name]
+            newrow = [x / 1000 for x in row]
+            T = np.vstack([T, newrow])
+            newcolumn = [[0], [0], [0], [1]]
+            T = np.hstack([T, newcolumn])
+            #print('yaw')
+        #print(T)
 
         return T
 
@@ -62,14 +131,14 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
 
         :param joints: {joint_name: joint_angle}
         '''
-        for chain_joints in self.chains.values():
+        for chain_joints in self.chains.values():  # gehe die values der chains durch (listen von joints)
             T = identity(4)
-            for joint in chain_joints:
+            for joint in chain_joints:  #gehe z.b. in head chain alle joints durch,
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
                 # YOUR CODE HERE
-
-                self.transforms[joint] = T
+                T = T * Tl #mulitpl. alle matrizen für letzten joint am arm z.b. zusammen
+                self.transforms[joint] = T #kriegen für jeden joint eine matrix relativ zu torso abgespeichert
 
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -12,31 +12,75 @@
 
 from forward_kinematics import ForwardKinematicsAgent
 from numpy.matlib import identity
-
+import numpy as np
+from numpy import random, linalg
+from scipy.optimize import fmin
 
 class InverseKinematicsAgent(ForwardKinematicsAgent):
     def inverse_kinematics(self, effector_name, transform):
         '''solve the inverse kinematics
 
         :param str effector_name: name of end effector, e.g. LLeg, RLeg
-        :param transform: 4x4 transform matrix
+        :param transform: 4x4 transform matrix, target matrix
         :return: list of joint angles
         '''
         joint_angles = []
         # YOUR CODE HERE
+
+        def error_func(angles, target, effect_name):
+            d = {j: 0.0 for j in self.joint_names} #erstelle dict mit joints und alle winkel auf 0.0
+            for i, j in enumerate(self.chains[effect_name]): #gehe alle joints in chain d. effector durch
+                d[j] = angles[i] #setze dict angles auf angles aus angles
+
+            #forw_kin_m = self.forward_kinematics(d) #aktueller wert durch forward mit akt. theta
+            self.forward_kinematics(d)
+            forw_kin_m = self.transforms[self.chains[effect_name][-1]]
+            e = target - forw_kin_m  #fehler zw. target und aktuellen werten, matrizen
+            return linalg.norm(e)
+
+        #func = lambda t: error_func(t, transform, effector_name)
+        joint_angles = fmin(        # fmin(function, x0, args...)
+            error_func,
+            np.random.rand(len(self.chains[effector_name])),
+            (transform, effector_name),
+            xtol=10**-9, ftol=10**-9)
         return joint_angles
 
     def set_transforms(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
-        self.keyframes = ([], [], [])  # the result joint angles have to fill in
+        # YOUR CODE HERE (erstelle keyframe um inverse bewegung auszuführen)
+        angles = self.inverse_kinematics(effector_name, transform)
+        names = []
+        times = []
+        keys = []
+        for j in self.joint_names:
+            names.append(j)
+            times.append([0.0, 1.0]) #aktuelle zeit, endzeit
+            key = [[self.target_joints[j], [3, -0.1, 0.0], [3, 0.1, 0.0]]]
+
+            #keys.append([self.target_joints[j], [3, -0.1, 0.0], [3, 0.1, 0.0]]) #initialer keyframe, start
+            if j in self.chains[effector_name]:
+                key += [[angles[self.chains[effector_name].index(j)], [3, -0.1, 0.0], [3, 0.1, 0.0]]]
+                keys.append(key)
+                #keys.append([angles[self.chains[effector_name].index(j)], [3, -0.1, 0.0], [3, 0.1, 0.0]]) #füge winkel aus inverse kinematics hinzu (ziel)
+            else:
+                key += [[self.target_joints[j], [3, -0.1, 0.0], [3, 0.1, 0.0]]]
+                keys.append(key)
+                #keys.append([self.target_joints[j], [3, 0.1, 0.0], [3, 0.1, 0.0]])
+        self.keyframes = (names, times, keys)
+
+
+        #self.keyframes = ([], [], [])  # the result joint angles have to fill in
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
     # test inverse kinematics
     T = identity(4)
+    #T[-1, 0] =
     T[-1, 1] = 0.05
-    T[-1, 2] = -0.26
+    T[-1, 2] = -0.2879
+        #-0.26
+
     agent.set_transforms('LLeg', T)
     agent.run()
